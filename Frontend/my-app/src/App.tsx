45469d6b012205966b640e77efa189f4b4a9813f
SaifBhuiya <bhuiyasaif@gmail.com>
2025-04-28 19:25:27 +0600

    frontend+test

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,17 +1,63 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
 
-          <div className="paste_Input">
-              <div>
+    const [descInput, setDescInput] = useState<string>('');
+    const [resumeInput, setResumeInput] = useState<string>('');
 
-              </div>
-          </div>
-    </div>
-  );
+   
+    const handleSubmit = () => {
+        fetch('http://127.0.0.1:8000/items',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'text/plain', },
+                body: descInput 
+            }
+        )
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    };
+
+
+
+    function generate_CL() {
+        handleSubmit()
+    }
+    function clear_Input() {
+        setDescInput("")
+        setResumeInput("")
+    }
+
+    return (
+        <div className="App">
+            <header>
+                Cover Letter Generator
+            </header>
+
+            <section>
+                <label>Job Description</label>
+                <textarea id="Job_Desc_Input" value={descInput} placeholder="paste/type Job Description" onChange={(e) => setDescInput(e.target.value)}></textarea>
+
+                <label>Resume Details</label>
+                <textarea id="Resume_Input" value={resumeInput} placeholder="paste/type Resume Details" onChange={(e) => setResumeInput(e.target.value)}></textarea>
+                <div>
+                    <button onClick={() => generate_CL()}>
+                        Generate Cover Letter
+                    </button>
+                    <button onClick={() => clear_Input()}>
+                        Clear
+                    </button>
+                </div>
+
+            </section>
+
+
+        </div>
+    );
 }
 
 export default App;
