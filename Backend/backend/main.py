3a6484baadb65a8a5e2ddea2327ef460caa44d12
SaifBhuiya <bhuiyasaif@gmail.com>
2025-04-29 22:54:33 +0600

    Update main.py

--- Backend/backend/main.py
+++ Backend/backend/main.py
@@ -63,7 +63,7 @@ origins=[
 # Adding CORS middleware to allow requests from any origin (for development purposes)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
