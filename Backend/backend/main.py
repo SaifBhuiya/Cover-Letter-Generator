cc4ab52975ae4ab1479bc212e0e24247aa41c4dc
SaifBhuiya <bhuiyasaif@gmail.com>
2025-04-29 22:23:06 +0600

    backend connection test

--- Backend/backend/main.py
+++ Backend/backend/main.py
@@ -13,7 +13,9 @@ client=OpenAI(
     base_url=OPENAI_BASE_URL,
     api_key=os.environ.get('GITHUB_TOKEN'),
 )
-
+@app.get("/test_connection")
+async def test_connection():
+    return {"status": "Connection to FastAPI is working!"}
 
 #Function to use LLM Model
 def model_reply(job_Desc, resume):
